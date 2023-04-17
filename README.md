5fc1d53cd555b15dc79d832999220bdbdca253c8
Christian Delicaat <c.delicaat@hotmail.com>
2023-04-17 19:16:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# HexMap
+
+Developed with Unreal Engine 5
---
+++ Source/HexMap/HexMapGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HexMapGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEXMAP_API AHexMapGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
